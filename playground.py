bd47cdfc257a26d4f249eb85b43e466912521ce5
rayzamgh <rayzaganteng@gmail.com>
2019-10-27 22:04:14 +0700

    pepegchange

--- playground.py
+++ playground.py
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 asawawu = ['AAA', 'BBB']
 
 for a in asawawu:
     eval(a + " = 1")
+=======
+import SteamTable as st
+import calculation as calc
+
+print(calc.liquidVelocityNumb(0.126, 896.92, 9.81, 0.044))
+>>>>>>> Stashed changes
