41f057ac4e1d7aa228f37ddcdd53125bfde5f434
aryuuu <herppratama@gmail.com>
2019-12-10 15:12:09 +0700

    tampilan output udah lumayan rapi, tombol option x dan y masih ada yang kurang, belum diintegrasi buat plot

--- output.py
+++ output.py
@@ -2,14 +2,28 @@
 
 import tkinter as tk
 
+#===========
 # CONSTANTS
+#===========
 height = 600
 width = 800
+option_button_height = 2
+option_button_width = 2
 
+#===========
+#GLOBAL VAR
+#===========
 # besaran pada sumbu
 x_val = "X"
 y_val = "Y"
 
+
+
+
+
+#================
+# event handler
+#================
 
 # fungsi yang dipanggil saat tombol export csv ditekan
 # menerima nama file yang akan diekpor dan menulis ke file system
@@ -32,79 +46,133 @@ def setPlot(axis, var):
 	print("just changed {} axis var to {}".format(axis, var))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# =====================
+# Tkinter window
+# =====================
 
 root = tk.Tk()
 root.title("Therwell")
 root.geometry('{}x{}'.format(width, height))
 root.configure(background = "#CBCBCB");
 
+
+#==========================================================
 # pembagian window jadi beberapa frame dengan spesialisasi
+#==========================================================
 
-frame_options = tk.Frame(root, width=width, height=100, background="red")
+frame_options = tk.Frame(root, width=width, height=75, background="#CBCBCB")
 frame_options.pack(fill=None, expand=False, side=tk.TOP)
+frame_options.pack_propagate(False)
 
-frame_plot = tk.Frame(root, width=width, height=400, background="green")
+frame_plot = tk.Frame(root, width=width, height=425, background="green")
 frame_plot.pack(fill=None, expand=False, side=tk.TOP)
 
-frame_exports = tk.Frame(root, width=width, height=100, background="yellow")
+frame_exports = tk.Frame(root, width=width, height=100, background="#CBCBCB")
 frame_exports.pack(fill=None, expand=False, side=tk.TOP)
 
 frame_label = tk.Frame(frame_exports, width=400, height=100, background="#CBCBCB")
 frame_label.pack(fill=None, expand=False, side=tk.LEFT)
+frame_label.pack_propagate(False)
 
-frame_export_buttons = tk.Frame(frame_exports, width=400, height=100, background="black")
+frame_export_buttons = tk.Frame(frame_exports, width=400, height=100, background="#CBCBCB")
 frame_export_buttons.pack(fill=None, expand=False, side=tk.RIGHT)
+frame_export_buttons.pack_propagate(False)
 
 
-# label untuk menaruh ouput dari csv
+#===============
+# tombol option
+#===============
 
-frame_label_whp = tk.Frame(frame_label, width=400, height=40, background="blue")
+frame_separator = tk.Frame(frame_options, width=width, height=15, background="#CBCBCB")
+frame_separator.pack(fill=None, expand=False, side=tk.TOP)
+
+frame_option_buttons = tk.Frame(frame_options, width=width, height=45, background="#CBCBCB")
+frame_option_buttons.pack(fill=None, expand=False, side=tk.TOP)
+frame_option_buttons.pack_propagate(0)
+
+frame_separator = tk.Frame(frame_option_buttons, width=50, height=45,background="#CBCBCB")
+frame_separator.pack(fill=None, expand=False, side=tk.LEFT)
+
+# label_x_var = tk.Label(frame_option_buttons, wraplength=1, text="x : ", bg="#CBCBCB",font="none 11 bold")
+# label_x_var.pack(fill=None)
+
+frame_x_var = tk.Frame(frame_option_buttons, width=120, height=45, background="#CBCBCB")
+frame_x_var.pack(fill=None, expand=False, side=tk.LEFT)
+frame_x_var.pack_propagate(0)
+
+frame_separator = tk.Frame(frame_option_buttons, width=50, height=45,background="#CBCBCB")
+frame_separator.pack(fill=None, expand=False, side=tk.LEFT)
+
+# label_y_var = tk.Label(frame_option_buttons, wraplength=1, text="x : ", bg="#CBCBCB",font="none 11 bold")
+# label_y_var.pack(fill=None)
+
+frame_y_var = tk.Frame(frame_option_buttons, width=120, height=45, background="#CBCBCB")
+frame_y_var.pack(fill=None, expand=False, side=tk.LEFT)
+frame_y_var.pack_propagate(0)
+
+
+button_x_p = tk.Button(frame_x_var, width=option_button_width, height=option_button_height, text="P", command=lambda: setPlot('x', 'P'))
+button_x_p.pack(fill=None, expand=False, side=tk.LEFT)
+
+button_x_v = tk.Button(frame_x_var, width=option_button_width, height=option_button_height, text="V", command=lambda: setPlot('x', 'V'))
+button_x_v.pack(fill=None, expand=False, side=tk.LEFT)
+
+button_y_t = tk.Button(frame_y_var, width=option_button_width, height=option_button_height, text='T', command=lambda: setPlot('y', 'T'))
+button_y_t.pack(fill=None, expand=False, side=tk.LEFT)
+
+button_y_pi = tk.Button(frame_y_var, width=option_button_width, height=option_button_height, text='pi', command=lambda: setPlot('y', 'PI'))
+button_y_pi.pack(fill=None, expand=False, side=tk.LEFT)
+
+
+#====================================
+# label untuk menaruh output dari csv
+#====================================
+
+frame_separator = tk.Frame(frame_label, width=400, height=20, background="#CBCBCB")
+frame_separator.pack(fill=None, expand=False, side=tk.TOP)
+
+frame_label_whp = tk.Frame(frame_label, width=400, height=40, background="#CBCBCB")
 frame_label_whp.pack(fill="both", expand=False, side=tk.TOP)
+frame_label_whp.pack_propagate(0)
 
-frame_label_pwf = tk.Frame(frame_label, width=400, height=40, background="purple")
+frame_label_pwf = tk.Frame(frame_label, width=400, height=40, background="#CBCBCB")
 frame_label_pwf.pack(fill="both", expand=False, side=tk.TOP)
+frame_label_pwf.pack_propagate(0)
 
-label_whp = tk.Label(frame_label_whp, text="WHP : ", bg="white", font="none 12 bold")
-label_whp.pack(fill=None)
+label_whp = tk.Label(frame_label_whp, text="WHP : ", bg="#CBCBCB", font="none 12 bold")
+label_whp.pack(fill="x")
 
-label_pwf = tk.Label(frame_label_pwf, text="PWF : ", bg="white", font="none 12 bold")
-label_pwf.pack(fill=None)
+label_pwf = tk.Label(frame_label_pwf, text="PWF : ", bg="#CBCBCB", font="none 12 bold")
+label_pwf.pack(fill="x")
 
+
+#===============
 # tombol export
+#===============
 
-frame_export_report = tk.Frame(frame_export_buttons, width=400, height=50, background="red")
+frame_separator = tk.Frame(frame_export_buttons, width=400, height=20, background="#CBCBCB")
+frame_separator.pack(fill=None, expand=False, side=tk.TOP)
+
+
+frame_export_report = tk.Frame(frame_export_buttons, width=300, height=30, background="#CBCBCB")
 frame_export_report.pack(fill=None, expand=False, side=tk.TOP)
+frame_export_report.pack_propagate(0)
 
-frame_export_csv = tk.Frame(frame_export_buttons, width=400, height=50, background="pink")
+frame_export_csv = tk.Frame(frame_export_buttons, width=300, height=30, background="#CBCBCB")
 frame_export_csv.pack(fill=None, expand=False, side=tk.TOP)
+frame_export_csv.pack_propagate(0)
 
-button_export_report = tk.Button(frame_export_report, width=15, height=2, text="Export Report" , bg="white" , command=lambda: exportReport("my ass.txt"))
-button_export_report.pack(fill="both")
+button_export_report = tk.Button(frame_export_report, width=15, height=1, text="Export Report" , bg="white" , command=lambda: exportReport("my ass.txt"))
+button_export_report.pack(fill=None)
 
-button_export_csv = tk.Button(frame_export_csv, width=15, height=2, text="Export CSV" , bg="white" , command=lambda:exportCSV("my ass.csv"))
-button_export_csv.pack(fill="both")
-
-
-
+button_export_csv = tk.Button(frame_export_csv, width=15, height=1, text="Export CSV" , bg="white" , command=lambda:exportCSV("my ass.csv"))
+button_export_csv.pack(fill=None)
 
 
-# buka window
+
+
+#=============
+# MAIN
+#=============
 
 root.mainloop()
